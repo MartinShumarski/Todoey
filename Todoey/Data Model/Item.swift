099155622e97ff66032311839ca6b31b37468b74
Martin Shumarski <ms@martin-shumarskis-macbook-pro.local>
2019-07-03 17:45:36 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Martin Shumarski on 3.07.19.
+//  Copyright © 2019 Martin Shumarski. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
