e78ede0e1e7a1c7294c254923ff7dae203d968b1
Martin Shumarski <ms@martin-shumarskis-macbook-pro.local>
2019-07-18 17:07:28 +0300

    Added searchBar functionality and query over the core data.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -20,7 +20,8 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         
          print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        loadItems()
+        
+         loadItems()
         
     }
         // Do any additional setup after loading the view, typically from a nib.
@@ -102,23 +103,49 @@ class TodoListViewController: UITableViewController {
         
         self.tableView.reloadData()
     }
-    func loadItems () {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems (with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+      
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context \(error)")
         }
+        
+        tableView.reloadData()
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
+    
+    
+    //MARK: Search Bar Methods
+    
+extension TodoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+       request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+
+        
+        loadItems(with: request)
+        
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+            
+        }
+    }
+}
+
+
+
+
